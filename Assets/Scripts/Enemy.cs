84549bdaf3ced8d59f956c053fe294a4f237145f
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-01 23:58:25 +0200

    Enemy speed added

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move down at 4 meters per second
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        //if bottom of screen, respawn at top with a new random x position
+
+    }
+}
