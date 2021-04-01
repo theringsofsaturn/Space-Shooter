1b618933c4f1f3f97555d624fbd0c7b0a1e97476
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-01 22:34:45 +0200

    Cool down system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,6 +13,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _firerate = 0.5f;
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,9 +31,9 @@ public class Player : MonoBehaviour
         CalculateMovement();
 
         //if I hit the space key, spawn game object
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
-            
+            _canFire = Time.time + _firerate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.7f, 0), Quaternion.identity);
         }
     }
