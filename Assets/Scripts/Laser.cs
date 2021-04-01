65640d9fba3e34d0bf98719a75f118ceb337d645
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-01 21:24:06 +0200

    Laser behaviour

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speedLaser = 8.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //Translate laser position up * speed * real time
+        transform.Translate(Vector3.up * _speedLaser * Time.deltaTime);
+    }
+}
