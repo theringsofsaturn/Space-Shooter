9ee004e3a1aa6402b445012a374ccfda08b9079e
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-01 21:37:35 +0200

    Destroy laser out of boundaries

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Laser : MonoBehaviour
 {
+    public float upBoundaryLaser;
     [SerializeField]
     private float _speedLaser = 8.0f;
 
@@ -18,5 +19,12 @@ public class Laser : MonoBehaviour
     { 
         //Translate laser position up * speed * real time
         transform.Translate(Vector3.up * _speedLaser * Time.deltaTime);
+
+        //When laser is greater than 8, destroy the laser
+        upBoundaryLaser = transform.position.y;
+        if (upBoundaryLaser >= 8)
+        {
+            Destroy(gameObject);
+        }
     }
 }
