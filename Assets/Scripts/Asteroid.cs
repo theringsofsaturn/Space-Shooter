6ea69575aaa48d6f0d88bbce718172c12c6293da
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-06 01:34:50 +0200

    Asteroid game object added. Script for rotation behaviour added

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+
+    [SerializeField]
+    private float rotateSpeed = 19.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
+    }
+}
