a5e138590f5b91a437dcee30d122647ff0e2a32f
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-06 02:17:15 +0200

    Explode the asteroid

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,6 +7,10 @@ public class Asteroid : MonoBehaviour
 
     [SerializeField]
     private float rotateSpeed = 19.0f;
+
+    [SerializeField]
+    private GameObject explosionPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,8 +23,21 @@ public class Asteroid : MonoBehaviour
         //rotate object on the zed axis
         transform.Rotate(Vector3.forward * rotateSpeed * Time.deltaTime);
 
-        //check for laser collision
-        //instantiate the asteroid at the position of the asteroid (us)
+        
+        
         //destroy explosion after 3 seconds
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //check for laser collision
+        if (other.tag == "Laser")
+        {
+            //instantiate the asteroid at the position of the asteroid (us)
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(this.gameObject, 0.25f);
+        }
     }
 }
